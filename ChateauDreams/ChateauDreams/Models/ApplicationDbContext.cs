aef3c3c90d82d6957a7c14d733d232a6bd0aa3e6
Alexander-Lazarov88 <alexander_lazarov88@abv.bg>
2016-08-12 23:29:57 +0300

    Some changes
    Enable Migrations
    Create a CommetsController and comment table
    Add button "Мнения"

---
+++ ChateauDreams/ChateauDreams/Models/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace ChateauDreams.Models
+{
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        public System.Data.Entity.DbSet<ChateauDreams.Models.Comments> Comments { get; set; }
+    }
+}
---
+++ ChateauDreams/ChateauDreams/Models/Comments.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ChateauDreams.Models
+{
+    public class Comments
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(300)]
+        public string Text { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        [Required]
+        public string Likes { get; set; }
+
+        public ApplicationUser Author { get; set; }
+    }
+}
