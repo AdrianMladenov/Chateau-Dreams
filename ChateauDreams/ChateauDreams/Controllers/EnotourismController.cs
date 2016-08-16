785a3a5c1dba16001d061c843eefd36cb09de219
Alexander-Lazarov88 <alexander_lazarov88@abv.bg>
2016-08-16 22:09:00 +0300

    Some changes in Controllers, Models and Views
    Add Dropdown menu; Add Controllers, Models and Views: Hotel and
    Enotourism

---
+++ ChateauDreams/ChateauDreams/Controllers/EnotourismController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChateauDreams.Controllers
+{
+    public class EnotourismController : Controller
+    {
+        public ActionResult WineHistory()
+        {
+            ViewBag.Message = "THIS IS FOR THE HISTORY OF WINE";
+
+            return View();
+        }
+
+        
+
+        public ActionResult EnotourismHistory()
+        {
+            ViewBag.Message = "THIS IS FOR THE HISTORY OF ENOTOURISM";
+
+            return View();
+        }
+
+        public ActionResult TastingRoom()
+        {
+            ViewBag.Message = "THIS IS FOR THE TASTING ROOM";
+
+            return View();
+        }
+
+    }
+}
--- ChateauDreams/ChateauDreams/Controllers/HomeController.cs
+++ ChateauDreams/ChateauDreams/Controllers/HomeController.cs
@@ -15,12 +15,7 @@ namespace ChateauDreams.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
+        
 
         public ActionResult Contact()
         {
