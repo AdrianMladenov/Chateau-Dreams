ad7ef52b04f051a14a4ce4bfada7cecbc66bd591
RoApostolova <zionbg@abv.bg>
2016-08-27 17:12:36 +0300

    ReservationsModel added

---
+++ ChateauDreams/ChateauDreams/Models/ReservationsModels.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ChateauDreams.Models
+{
+    public class ReservationsModels
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int RoomId { get; set; }
+
+        [Required]
+        public DateTime CheckInDate { get; set; }
+
+        [Required]
+        public DateTime CheckOutDate { get; set; }
+    }
+}
