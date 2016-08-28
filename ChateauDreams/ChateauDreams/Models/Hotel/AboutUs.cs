b288712f931eef20cbd633221a9c50436521111b
Alexander-Lazarov88 <alexander_lazarov88@abv.bg>
2016-08-28 08:43:39 +0300

    Ordered models, controllers and views

---
+++ ChateauDreams/ChateauDreams/Models/Hotel/AboutUs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ChateauDreams.Models.Hotel
+{
+    public class AboutUs
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(50)]
+        public string Title { get; set; }
+
+        [Required]
+        [DataType(DataType.MultilineText)]
+        public string Body { get; set; }
+    }
+}
