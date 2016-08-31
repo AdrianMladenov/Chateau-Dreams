2f38b74f4ce3e682facbbd2fae07dea18467b4b2
RoApostolova <zionbg@abv.bg>
2016-08-31 13:14:56 +0300

    Created 3 rooms, Greeting "Hello" changed to "Good Morning","Good Evening" etc., depending on the time you log in.

--- ChateauDreams/ChateauDreams/Controllers/Admin/RoomsController.cs
+++ ChateauDreams/ChateauDreams/Controllers/Admin/RoomsController.cs
@@ -47,7 +47,7 @@ namespace ChateauDreams.Controllers.Admin
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Title,Description,Price")] Rooms rooms)
+        public ActionResult Create([Bind(Include = "Id,Type,Description,Price")] Rooms rooms)
         {
             if (ModelState.IsValid)
             {
@@ -79,7 +79,7 @@ namespace ChateauDreams.Controllers.Admin
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Title,Description,Price")] Rooms rooms)
+        public ActionResult Edit([Bind(Include = "Id,Type,Description,Price")] Rooms rooms)
         {
             if (ModelState.IsValid)
             {
